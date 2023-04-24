689664e0c3f667b03c435fc4a6406b0d4854ad16
Wendy Chen <wchen242@se14l0120-08.ad.bcit.ca>
2023-04-24 14:22:19 -0700

    adding files

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/About.module.css';
+
+export default function About() {
+    return(
+        <>
+        <main className={styles.main}>
+            About Page
+        </main>
+        </>
+    ) 
+}
